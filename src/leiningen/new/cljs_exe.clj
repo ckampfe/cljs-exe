a1a9406e6609bc8f11d9bcccc33e58c915139b19
Clark Kampfe <clark.kampfe@gmail.com>
2017-01-22 21:30:34 -0600

    rm foo

--- src/leiningen/new/cljs_exe.clj
+++ src/leiningen/new/cljs_exe.clj
@@ -10,10 +10,10 @@
               :sanitized (name-to-path name)}]
     (println "Generating fresh 'boot new' cljs-exe project.")
     (->files data
-             ["README.md"                        (render "README.md" data)]
-             ["src/cljs/{{sanitized}}/core.cljs" (render "core.cljs" data)]
+             ["README.md"                        (render "README.md"    data)]
              ["resources/js/app.cljs.edn"        (render "app.cljs.edn" data)]
              ["build.boot"                       (render "build.boot"   data)]
              ["scripts/build.js"                 (render "build.js"     data)]
+             ["src/cljs/{{sanitized}}/core.cljs" (render "core.cljs"    data)]
              ["package.json"                     (render "package.json" data)]
              ["yarn.lock"                        (render "yarn.lock"    data)])))
